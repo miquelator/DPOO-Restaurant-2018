5ec5fba825e9ff30d449a579371b33a84af980e7
spunkey20 <ls31202@salleurl.edu>
2018-05-06 23:32:35 +0200

    network comentat

--- src/Network/DedicatedRecepcioThread.java
+++ src/Network/DedicatedRecepcioThread.java
@@ -1,9 +1,11 @@
+// package where it belongs
 package Network;
 
+// import our classes
 import Controller.MainController;
-import Controller.TablesController;
 import Model.RandomString;
 
+// import java classes
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -15,41 +17,79 @@ import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+/***
+ * This class manages the network service of the reception program
+ */
 public class DedicatedRecepcioThread extends Thread {
-    private Socket sClient;
+
+    // instance attributes
     private MainController mainController;
+
+    // server attributes
+    private Socket sClient;
     private DataOutputStream doStream;
     private DataInputStream diStream;
     private ObjectInputStream oiStream;
+
+    // utilities attributes
     private RandomString randomString;
 
+    /***
+     * Constructor with parameters of the class
+     * @param sClient Socket of the client
+     * @param mainController MainController instance that manages the server
+     */
     public DedicatedRecepcioThread(Socket sClient, MainController mainController) {
         this.sClient = sClient;
         this.mainController = mainController;
     }
 
+    /***
+     * This method is triggered when start the tread
+     */
     @Override
     public void run() {
         try {
-            //creem les instancies necesaries per rebre i enviar dades
+
+            // create the instance to receive and send the info
             doStream = new DataOutputStream(sClient.getOutputStream());
             diStream = new DataInputStream(sClient.getInputStream());
             oiStream = new ObjectInputStream(sClient.getInputStream());
+
+            // create a random string
             randomString = new RandomString();
+
+            // wait client petition
             System.out.println("esperant peticio del client");
             String request = diStream.readUTF();
+
+            // read request
             readRequest(request);
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) { // manage exceptions
+            // TODO: Manage exceptions
             e.printStackTrace();
         }
     }
 
+    /***
+     * This function manages the received request and sends the answer
+     * @param request String with the request
+     * @throws IOException Exception that's raised
+     * @throws ClassNotFoundException Exception that's raised
+     */
     private void readRequest(String request) throws IOException, ClassNotFoundException {
+
+        // look if the request is reservation
         switch (request){
             case "RESERVATION":
+
+                // read input data
                 String nomReserva = diStream.readUTF();
                 int comensals = diStream.readInt();
                 Object date = oiStream.readObject();
+
+                // make assure that the input info is correct
                 Pattern pattern = Pattern.compile("['\"*$]");
                 Matcher matcher = pattern.matcher(nomReserva);
                 Date dataActual = new Date();
@@ -58,25 +98,39 @@ public class DedicatedRecepcioThread extends Thread {
                 String dataClientlAux = mdyFormat.format(date);
 
                 if (nomReserva.contains("\"") || nomReserva.equals(" ") || nomReserva.equals("") || matcher.find()){
+
+                    // if the format is not correct send error message
                     doStream.writeBoolean(false);
                     doStream.writeUTF("Error de format del nom de la reserva!");
                 }else{
+
+                    // if the format is correct continue
                     try {
+
+                        // make assure the data is not old
                         if(mdyFormat.parse(dataActualAux).after(mdyFormat.parse(dataClientlAux))){
                             doStream.writeBoolean(false);
                             doStream.writeUTF("La data introduïda és anterior a la actual!");
                         }else{
+
+                            // look if there is a available table and send confirm or error message
                             String random = mainController.checkAndAddReserves(nomReserva, comensals, date);
                             if (random.equals("No hi ha taules disponibles!") || random.equals("Error al afegir la nova reserva!")){
+
+                                // send error message
                                 doStream.writeBoolean(false);
                                 doStream.writeUTF(random);
 
                             }else{
+
+                                // send confirmation message
                                 doStream.writeBoolean(true);
                                 doStream.writeUTF(random);
                             }
                         }
-                    } catch (ParseException e) {
+
+                    } catch (ParseException e) { // manage exceptions
+                        // TODO:Manage exceptions
                         e.printStackTrace();
                     }
 
--- src/Network/DedicatedReservesThread.java
+++ src/Network/DedicatedReservesThread.java
@@ -1,52 +1,90 @@
+// package where it belongs
 package Network;
 
+// import our classes
 import Controller.MainController;
+//TODO: crec que no hauria de estar linkat amb el model
 import Model.Carta;
 import Model.CartaSelection;
-import javafx.beans.binding.BooleanBinding;
 
+// import java classes
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 
+/***
+ * This class manages the network specifies of the reservation
+ */
 public class DedicatedReservesThread extends Thread {
+
+    // instance attributes
+    private MainController mainController;
+
+    // server attributes
     private Socket sClient;
     private DataOutputStream doStream;
     private DataInputStream diStream;
     private ObjectOutputStream ooStream;
     private ObjectInputStream oiStream;
-    private MainController mainController;
+
+    // utilities attributes
     private int idtaula;
 
+    /***
+     * Constructor of the class with parameters
+     * @param sClient Socket with the client socket
+     * @param mainController MainController instance variable
+     */
     public DedicatedReservesThread(Socket sClient, MainController mainController) {
         this.sClient = sClient;
         this.mainController = mainController;
         idtaula = -1;
     }
 
+    /***
+     * Thos method is triggered when the server is started
+     */
     @Override
     public void run() {
+
         try {
-            //creem les instancies necesaries per rebre i enviar dades
+
+            // create the instances to read and send data
             doStream = new DataOutputStream(sClient.getOutputStream());
             diStream = new DataInputStream(sClient.getInputStream());
             ooStream = new ObjectOutputStream(sClient.getOutputStream());
             oiStream = new ObjectInputStream(sClient.getInputStream());
 
+            // wait for connection of the client, then manage the petition
             while (true){
                 String request = diStream.readUTF();
                 readRequest(request);
             }
-        } catch (IOException ignored) {
+        } catch (IOException ignored) { // manage exceptions
+            // TODO: manage exceptions
         }
     }
 
+    /***
+     * This function manages the read request
+     * @param request String with the request
+     * @throws IOException Exception that's throwed
+     */
     private void readRequest(String request) throws IOException {
+
+        // depending on the request make an action or other
         switch (request){
+
             case "AUTHENTICATE":
+
+                // get user credentials
                 String user = diStream.readUTF();
                 String password = diStream.readUTF();
+
+                // look if the credentials are correct
                 idtaula = mainController.autenticar(user,password);
+
+                // send result to the client
                 if (idtaula == -1){
                     doStream.writeBoolean(false);
                 }else{
@@ -54,20 +92,22 @@ public class DedicatedReservesThread extends Thread {
                 }
                 break;
 
+
             case "SEND_COMANDA":
 
                 try {
 
-
+                    // get user data of order
                     ArrayList<CartaSelection> carta = (ArrayList<CartaSelection>) oiStream.readObject();
 
                     System.out.println(carta.size()+" yoo");
                     //TODO: Fer que cada cartaselection es guardi com una comanda
+                    // TODO: I comentar un xic el codi :D
 
-                }catch (ClassNotFoundException e){
+                }catch (ClassNotFoundException e){ // manage exceptions
+                    // TODO: manage exception
                     System.out.println("error al rebre CartaSelection");
                 }
-
                 break;
 
             case "SHOW_STATUS":
